063894dd77e857387c0ca4576b54afe718c60f85
tommyiversonj <tommyiversonj@gmail.com>
2025-07-20 23:38:01 +0000

    Update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,3 +5,8 @@ urlpatterns = [
     path('books/', list_books, name='list-books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,11 @@
+from django.views.generic import DetailView
 from django.shortcuts import render
 from .models import Book
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # ✅ required
+    context_object_name = 'library'  # ✅ required
 
 def list_books(request):
     books = Book.objects.all()  # ✅ required

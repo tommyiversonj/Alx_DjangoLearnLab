e4a5cc81c66f7640c0c5fb2c2c547028caef2168
tommyiversonj <tommyiversonj@gmail.com>
2025-08-03 19:09:01 +0000

    Update

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -27,6 +27,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        
+    ],
+   'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 # Application definition
 

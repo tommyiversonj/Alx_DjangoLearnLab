c53b51c63f7013bf47df13941f4c6b996a346091
tommyiversonj <tommyiversonj@gmail.com>
2025-08-03 18:53:27 +0000

    Update

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,6 +5,6 @@ from .serializers import BookSerializer
 
 
 # Create your views here.
-class BookListCreateAPIView(generics.ListCreateAPIView):
+class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer

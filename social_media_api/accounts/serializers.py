126459306fd9712e55e30d5cbea0e3230c72e778
tommyiversonj <tommyiversonj@gmail.com>
2025-08-24 16:17:54 +0000

    Update

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,24 +4,23 @@ from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
+        # Use get_user_model().objects.create_user to create the user properly
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email', ''),
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None)
+            profile_picture=validated_data.get('profile_picture', None),
         )
-        token = Token.objects.create(user=user)
-        return user  # Return only user; token can be accessed in the view
+        return user
 
 
 class UserLoginSerializer(serializers.Serializer):
@@ -30,14 +29,19 @@ class UserLoginSerializer(serializers.Serializer):
     token = serializers.CharField(read_only=True)
 
     def validate(self, data):
-        user = authenticate(username=data['username'], password=data['password'])
-        if user and user.is_active:
-            token, _ = Token.objects.get_or_create(user=user)
-            return {'username': user.username, 'token': token.key}
-        raise serializers.ValidationError("Invalid credentials.")
+        username = data.get('username')
+        password = data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user and user.is_active:
+                token, _ = Token.objects.get_or_create(user=user)
+                return {'username': user.username, 'token': token.key}
+            raise serializers.ValidationError("Unable to log in with provided credentials.")
+        raise serializers.ValidationError("Must include 'username' and 'password'.")
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'bio', 'profile_picture', 'followers')
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']

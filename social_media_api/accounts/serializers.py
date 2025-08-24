cad1af81bf9aa14241d9b1a807d6f93518e7d015
tommyiversonj <tommyiversonj@gmail.com>
2025-08-24 16:57:44 +0000

    Update

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,16 +1,10 @@
 # accounts/serializers.py
 from rest_framework import serializers
 from django.contrib.auth import authenticate, get_user_model
-from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    """
-    Serializer for user registration.
-    Handles creation of a new CustomUser instance.
-    The password field is write-only for security.
-    """
     password = serializers.CharField(write_only=True)
 
     class Meta:
@@ -18,25 +12,14 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        """
-        Creates and returns a new user instance, given the validated data.
-        The token is not created here; it's handled by the view.
-        """
         user = User.objects.create_user(**validated_data)
         return user
 
 class UserLoginSerializer(serializers.Serializer):
-    """
-    Serializer for user login.
-    Validates username and password.
-    """
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
-        """
-        Custom validation to authenticate the user.
-        """
         username = data.get('username')
         password = data.get('password')
 
@@ -47,13 +30,9 @@ class UserLoginSerializer(serializers.Serializer):
         if not user:
             raise serializers.ValidationError("Unable to log in with provided credentials.")
         
-        # Return the user instance to be used by the view
         return user
 
 class UserSerializer(serializers.ModelSerializer):
-    """
-    Serializer for retrieving and displaying user data.
-    """
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']

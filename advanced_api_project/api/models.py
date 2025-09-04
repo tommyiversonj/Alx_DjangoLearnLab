84481bd532e58ce5ca3da7a77846a83f546ded57
tommyiversonj <tommyiversonj@gmail.com>
2025-09-04 15:39:39 +0000

    Update

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,18 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class Author(models.Model):
+    name = models.CharField(max_length=200)
 
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
 
 class Book(models.Model):
-    
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.title} ({self.publication_year})"
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
